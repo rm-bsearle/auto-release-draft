14ad84bfeaf8fdc840a899e9a33ac725722f2462
Searle, Brian <BrianSearle@rocketmortgage.com>
2024-05-30 15:00:00 -0400

    Adding initial draft

---
+++ src/git.ts
@@ -0,0 +1,67 @@
+import { exec } from '@actions/exec'
+import * as core from '@actions/core'
+
+export async function getChangesIntroducedByTag(
+    tag: string): Promise<string> {
+        const previousVersionTag = await getPreviousVersionTag(tag)
+
+    return  previousVersionTag
+      ? getCommitMessagesBetween(previousVersionTag, tag)
+      : getCommitMessagesFrom(tag)
+}
+
+export async function getPreviousVersionTag(
+    tag: string): Promise<string | null> {
+    let previousTag = ''
+
+    const options: ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                previousTag += data.toString()
+            }
+        },
+        silent: true,
+        ignoreReturnCode: true
+    }
+    
+    const exitCode = await exec(
+        'git',
+        ['describe',
+         '--match', 'v[0-9]*',
+         '--abbrev=0',
+         '--first-parent',
+         `${tag}^`],
+         options
+    );
+
+    core.debug(`The previous version tag is ${previousTag}`)
+
+    return exitCode === 0 ? previousTag.trim() : null
+}
+
+export async function getCommitMessagesBetween(
+    firstTag: string,
+    secondTag: string): Promise<string> {
+    let commitMessages = ''
+
+    const options: ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                commitMessages += data.toString()
+            }
+        },
+        silent: true,
+    }
+    
+    await exec(
+        'git',
+        ['log',
+         '--format=%s',
+         `${firstTag}..${secondTag}`],
+         options
+    );
+
+    core.debug(`The commit messages between ${firstTag}} and ${secondTag}`)
+
+    return commitMessages.trim();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as event from './event'
+import * as version from './version'
+import * as git from './git'
 
 /**
  * The main function for the action.
@@ -7,18 +9,13 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const tag = event.getCreatedTag()
+    if (tag && version.isSemVer(tag)) {
+      const changelog = await git.getChangesIntroducedByTag(tag)
+    }
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
